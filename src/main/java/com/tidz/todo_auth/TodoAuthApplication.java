cd62e7d43e191d081b72742ff25ed308f2183835
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-05 10:31:39 -0300

    first commit

---
+++ src/main/java/com/tidz/todo_auth/TodoAuthApplication.java
@@ -0,0 +1,13 @@
+package com.tidz.todo_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidz/todo_auth/TodoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.todo_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
